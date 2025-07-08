05b3f92e699b72b33c126e88ffea4ce474af391c
tanishkaD23 <tanishkadeshmukh23@gmail.com>
2025-07-08 15:53:56 +0530

    Added/Updated main.py file

--- main.py
+++ main.py
@@ -23,3 +23,66 @@ with st.expander("What's this?"):
          -Show a simple reading clarity score.
         Just for learning ,Not a daignosis !
     """)
+     def capture_gaze (duration = 15):
+          gaze = GazeTracking()
+          cam = cv2.VideoCapture(0)
+          gaze_list = []
+          start = time.time()
+
+          while time.time()-start < duration:
+               ret,frame = cam.read()
+               if not ret:
+                    break
+               
+               gaze.refresh(frame)
+               if gaze.is_blinking():gaze_list.append("Blinking")
+               elif gaze.is_right():gaze_list.append("Right")
+               elif gaze.is_left():gaze_list.append("left")
+               elif gaze.is_center():gaze_list.append("center")
+               else:gaze_list.append("Unknown")
+
+               cam.release()
+               return gaze_list
+          
+          def record_voice(duration=15,passage=""):
+               recog = sr.Recognizer()
+               expected_words = passage.lower().replace('.',"").replaceeplace(',', '').split()
+               said_text = " "
+
+               with sr.Microphone() as source:
+                    recog.adjust_for_ambient_noise(source)
+                    try:
+                         audio = recog.listen(source,timeout=duration,phrase_time_limit=duration)
+                         said_text = recog.recognize_google(audio)
+                         spoken = said_text.lower().replace('.',"").replace('.',"").split()
+                         score = sum(1 for word in expected_words if any(word in w for w in spoken ))
+                    except Exception as e:
+                     said_text = f"Error:{str(e)}"
+                    score = 0
+
+                    return said_text,score
+               def save_csv(text,score,gaze_summary,risk_level):
+                    with open("dyslexia_results.csv","a",newline="",encoding="utf-8") as f:
+                      writer = csv.writer(f)
+                      writer.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), text, score, gaze_summary, risk_level]) 
+
+               st.subheader("Pick a passage to Read")
+               passages = {
+                   "EASY":"The cat sat on the mat.It was a sunny day.",
+                   "MEDIUM":"Reading books is a great way to learn and grow your mind.",
+                   "HARD":"Despite difficulties,perseverance and hard work lead to success."}
+               level = st.selectbox("choose Level",list(passages.keys()))
+               st.success(passages[level])
+                
+               
+
+
+                                               
+
+                          
+
+
+               
+
+          
+     
