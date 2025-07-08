89f28f966dc294c99c76a8a84fbb3b8405446118
Divyanshi Sen <divyanshisen66@gmail.com>
2025-07-08 12:55:23 +0530

    added

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import streamlit as st # type: ignore
-import cv2 
-from gaze_tracking import GazeTracking
+import cv2  # type: ignore
+from gaze_tracking import GazeTracking # type: ignore
 import   speech_recognition as sr
 from concurrent.futures import ThreadPoolExecutor
 import time
 import csv
 from datetime import datetime
-import pandas as pd 
-import matplotlib as plt
+import pandas as pd  # type: ignore
+import matplotlib as plt # type: ignore
+
+st.set_page_config(page_title ="Dyslexia Screener",page_icon="📖",layout ="centered")
+st.title("🧠Dyslexia Eye and Reading Test")
+st.caption("Track your reading and voice clarity. Not a medical tool!")
+st.divider()
 
 
